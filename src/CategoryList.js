227119eb30ef8c7a51aae746eee21210420047cf
bsralkan <bsr.alkan@gmail.com>
2022-03-06 22:17:21 +0300

    arrow functions

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -9,13 +9,16 @@ export default class CategoryList extends Component {
       ],
       currentCategory: ""
     };
+    changeCategory = (category) => {
+      this.setState({currentCategory: category.categoryName});
+    }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem onClick={() => this.setState({currentCategory: category.categoryName})} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
           ))}
         </ListGroup>
         <h3>{this.state.currentCategory}</h3>
