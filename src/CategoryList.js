f2749de3a59076779eb193bf5c70036cae73ffe7
bsralkan <bsr.alkan@gmail.com>
2022-03-06 21:53:42 +0300

    state with constructor

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props){
-    super(props); 
-    state:{}
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
   }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
