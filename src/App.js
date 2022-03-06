1f3815f7a7a051a1930a570c88b9db3beadddd08
bsralkan <bsr.alkan@gmail.com>
2022-03-06 22:29:28 +0300

    transmission of data between components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 
-function App() {
-  let productInfo = {title: "Product List"} ;  
-  let categoryInfo = {title: "Category List"} ;
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo}  />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    currentCategory: ""
+  };  
 
-export default App;
+  changeCategory = (category) => {
+    this.setState({currentCategory: category.categoryName});
+  }
+  render() {
+    let productInfo = {title: "Product List"} ;  
+    let categoryInfo = {title: "Category List"} ;
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo}  />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
-export default class CategoryList extends Component {
- state = {
+  export default class CategoryList extends Component {
+  state = {
       categories: [
         { categoryId: 1, categoryName: "Beverages" },
         { categoryId: 2, categoryName: "Condiments" },
       ],
       currentCategory: ""
     };
-    changeCategory = (category) => {
-      this.setState({currentCategory: category.categoryName});
-    }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem onClick={() => this.props.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
           ))}
         </ListGroup>
-        <h3>{this.state.currentCategory}</h3>
+        <h3>{this.props.currentCategory}</h3>
       </div>
     );
   }
