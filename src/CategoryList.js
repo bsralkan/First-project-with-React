7cf8ff90da8037fb3323ebdc1f2687eb22e72f81
bsralkan <bsr.alkan@gmail.com>
2022-03-08 23:03:33 +0300

    added json server api for getting category list

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,19 +3,27 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 
   export default class CategoryList extends Component {
   state = {
-      categories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiments" },
-      ],
+      categories: [],
       currentCategory: ""
     };
+
+  componentDidMount(){
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    fetch('http://localhost:3000/categories')
+    .then(response => response.json())
+    .then(data => this.setState({categories: data}));
+  }
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem onClick={() => this.props.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem onClick={() => this.props.changeCategory(category)} key={category.id}>{category.categoryName}</ListGroupItem>
           ))}
         </ListGroup>
         <h3>{this.props.currentCategory}</h3>
