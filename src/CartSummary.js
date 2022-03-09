c089ab0c782ba55008d10bbea3712981bcc75b61
bsralkan <bsr.alkan@gmail.com>
2022-03-09 22:16:43 +0300

    added empty cart control

--- src/CartSummary.js
+++ src/CartSummary.js
@@ -4,28 +4,44 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
+  NavItem,
+  NavLink,
   UncontrolledDropdown,
 } from "reactstrap";
 
 export default class CartSummary extends Component {
+  renderSummary() {
+    return (
+      <UncontrolledDropdown inNavbar nav>
+        <DropdownToggle caret nav>
+          Your Cart
+        </DropdownToggle>
+        <DropdownMenu end>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+  renderEmptyCart(){
+      return(
+          <NavItem>
+              <NavLink>
+                  Empty Cart
+              </NavLink>
+          </NavItem>
+      )
+  }
   render() {
     return (
       <div>
-        <UncontrolledDropdown inNavbar nav>
-          <DropdownToggle caret nav>
-            Your Cart
-          </DropdownToggle>
-          <DropdownMenu end>
-            {this.props.cart.map((cartItem) => (
-              <DropdownItem key={cartItem.product.id}>
-                {cartItem.product.productName}
-                <Badge color="success">{cartItem.quantity}</Badge>
-              </DropdownItem>
-            ))}
-            <DropdownItem divider />
-            <DropdownItem>Reset</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
+        {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmptyCart()}
       </div>
     );
   }
