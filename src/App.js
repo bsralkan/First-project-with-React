669ceed262b42d79839212fec61c744ea687d000
bsralkan <bsr.alkan@gmail.com>
2022-03-09 21:40:46 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -35,9 +35,7 @@ export default class App extends Component {
     return (
       <div>
         <Container>
-          <Row>
             <Navi />
-          </Row>
           <Row>
             <Col xs="3">
               <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
--- src/Navi.js
+++ src/Navi.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
 
 export default class Navi extends Component {
   render() {
     return (
       <div>
-        <h2>Navi Component</h2>
+        <Navbar color="light" expand="md" light>
+          <NavbarBrand href="/">Northwind App</NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() {}} />
+          <Collapse navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown inNavbar nav>
+                <DropdownToggle caret nav>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
-    )
+    );
   }
 }
